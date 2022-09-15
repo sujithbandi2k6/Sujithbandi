2e9b8a3925aa32f7d93f6ee8514e727fe0b8646b
sujithbandi2k6 <sujithbandi2000@gmail.com>
2022-09-16 01:51:58 +0530

    Done

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ public class Demo {
     public static void main(String args[]) {
         System.out.println("Running system program with jenkins "+new Date());
         System.out.println("Running javaprogram for Sujith Bandi");
+        System.out.println("Welcome Once Again with Git And Jenkins");
     }
 }
